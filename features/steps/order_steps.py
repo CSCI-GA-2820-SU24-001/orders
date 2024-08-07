69af7c8370d42fcc391dde65ab44e5c961757a66
Divyanshu Sharma <sharmadivyanshu1709@gmail.com>
2024-08-07 09:51:05 +0530

    Add background for BDD
    Add background for BDD

--- features/steps/order_steps.py
+++ features/steps/order_steps.py
@@ -45,18 +45,59 @@ def step_impl(context):
     # and delete them one by one
     for order in context.resp.json():
         context.resp = requests.delete(
-            f"{rest_endpoint}/{order['order_id']}", timeout=WAIT_TIMEOUT
+            f"{rest_endpoint}/{order['id']}", timeout=WAIT_TIMEOUT
         )
         expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
 
     # load the database with new orders
     for row in context.table:
         payload = {
-            "items": row["items"],
+            "items": [],
             "customer_id": row["customer_id"],
             "shipping_address": row["shipping_address"],
             "created_at": row["created_at"],
             "status": row["status"],
         }
+        payload["items"].append(
+            {
+                "product_id": 1,
+                "order_id": 0,
+                "quantity": 2,
+                "price": 23.4,
+                "product_description": "Glucose",
+            }
+        )
         context.resp = requests.post(rest_endpoint, json=payload, timeout=WAIT_TIMEOUT)
         expect(context.resp.status_code).equal_to(HTTP_201_CREATED)
+
+
+@given("the following items")
+def step_impl(context):
+    """Delete all items and load new ones"""
+
+    # Get a list all of the orders
+    rest_endpoint = f"{context.base_url}/orders"
+    context.resp = requests.get(rest_endpoint, timeout=WAIT_TIMEOUT)
+    expect(context.resp.status_code).equal_to(HTTP_200_OK)
+    # and delete them one by one
+    for order in context.resp.json():
+        for item in order["items"]:
+            if item:
+                print("Inside item")
+                context.resp = requests.delete(
+                    f"{rest_endpoint}/{order['id']}/item/{item['id']}",
+                    timeout=WAIT_TIMEOUT,
+                )
+                expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
+
+    # load the database with new items
+    # for row in context.table:
+    #     payload = {
+    #         "order_id": int(row["order_id"]),
+    #         "product_id": int(row["product_id"]),
+    #         "price": float(row["price"]),
+    #         "quantity": int(row["quantity"]),
+    #         "product_description": row["product_description"],
+    #     }
+    #     context.resp = requests.post(rest_endpoint, json=payload, timeout=WAIT_TIMEOUT)
+    #     expect(context.resp.status_code).equal_to(HTTP_201_CREATED)
