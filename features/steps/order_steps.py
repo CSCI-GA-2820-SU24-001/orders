4938fc12e921de45e132db630c282cd3f25714a1
Divyanshu Sharma <sharmadivyanshu1709@gmail.com>
2024-08-07 09:55:46 +0530

    Update order_steps.py

--- features/steps/order_steps.py
+++ features/steps/order_steps.py
@@ -76,19 +76,19 @@ def step_impl(context):
     """Delete all items and load new ones"""
 
     # Get a list all of the orders
-    rest_endpoint = f"{context.base_url}/orders"
-    context.resp = requests.get(rest_endpoint, timeout=WAIT_TIMEOUT)
-    expect(context.resp.status_code).equal_to(HTTP_200_OK)
-    # and delete them one by one
-    for order in context.resp.json():
-        for item in order["items"]:
-            if item:
-                print("Inside item")
-                context.resp = requests.delete(
-                    f"{rest_endpoint}/{order['id']}/item/{item['id']}",
-                    timeout=WAIT_TIMEOUT,
-                )
-                expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
+    # rest_endpoint = f"{context.base_url}/orders"
+    # context.resp = requests.get(rest_endpoint, timeout=WAIT_TIMEOUT)
+    # expect(context.resp.status_code).equal_to(HTTP_200_OK)
+    # # and delete them one by one
+    # for order in context.resp.json():
+    #     for item in order["items"]:
+    #         if item:
+    #             print("Inside item")
+    #             context.resp = requests.delete(
+    #                 f"{rest_endpoint}/{order['id']}/item/{item['id']}",
+    #                 timeout=WAIT_TIMEOUT,
+    #             )
+    #             expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
 
     # load the database with new items
     # for row in context.table:
